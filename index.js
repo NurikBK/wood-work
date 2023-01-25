80ead33873d3cebc906e98eb56cb84a0b98b840d
Nurzhan Bizhan <bizhannk@gmail.com>
2023-01-25 10:03:52 +0600

    change animation

--- index.js
+++ index.js
@@ -24,3 +24,17 @@ cards.forEach((card) => {
 })
 
 setClasses();
+
+const observer = new  IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } 
+    // else {
+    //   entry.target.classList.remove('show')
+    // }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => observer.observe(el))
